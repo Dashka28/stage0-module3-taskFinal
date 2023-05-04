dc4aabf17f1028dc54209656a24d978256384a89
Dashka28 <131460708+Dashka28@users.noreply.github.com>
2023-05-04 18:01:23 +0600

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int hundreds = (int)(number/100);
+        int remaining = number-100*hundreds;
+        int dec = (int)(remaining /10);
+        remaining -= 10*dec;
+
+        int reversed = 100*remaining + 10*dec + hundreds;
+
+        System.out.println(reversed);
     }
 }
